327d66d64428fe0a290b2de0bc67659b511ed942
saurabh <saurabh7481@gmail.com>
2022-02-21 19:01:12 +0530

    added getelementbytagname

--- main.js
+++ main.js
@@ -7,10 +7,18 @@
 // header2.style.color = "green";
 
 // GetElementByClassName
-const items = document.getElementsByClassName("list-group-item");
-console.log(items);
-items[2].style.backgroundColor = "green";
+// const items = document.getElementsByClassName("list-group-item");
+// console.log(items);
+// items[2].style.backgroundColor = "green";
 
-for(let el of items){
-    el.style.fontWeight = "bold";
-}
+// for(let el of items){
+//     el.style.fontWeight = "bold";
+// }
+
+// GetElementByTagName
+const li = document.getElementsByTagName("li");
+li[2].style.backgroundColor = "green";
+
+// this will not work
+const li_class = document.getElementsByClassName("list-group-item");
+li_class[4].style.backgroundColor = "blue"; 
