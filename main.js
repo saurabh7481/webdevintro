a59a04581594d68733ea7ee1e33f2d916e7dc168
saurabh <saurabh7481@gmail.com>
2022-02-21 16:59:51 +0530

    Added styles

--- main.js
+++ main.js
@@ -1,25 +1,6 @@
-const form = document.querySelector("#form");
-const username = document.querySelector("#name");
-const email = document.querySelector("#email");
+const header = document.querySelector("#header-title");
+header.style.borderBottom = "solid 2px blue";
 
-// form.addEventListener('click', (e) => {
-//     e.preventDefault();
-//     console.log("clicked")
-// })
-
-// form.addEventListener("mouseover" , (e) => {
-//     alert("mouseover");
-// })
-
-// form.addEventListener("mouseout", (e) => {
-//     alert("mouseout");
-// })
-
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    if(username.value == "" || email.value == ""){
-        alert("Enter values!")
-    } else {
-        console.log(username.value, email.value);
-    }
-})
+const header2 = document.querySelector(".title");
+header2.style.fontWeight = "bold";
+header2.style.color = "green";
