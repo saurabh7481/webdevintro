d5daf3f119ecdfabb780634e63794ccf0ba6fd7c
saurabh <saurabh7481@gmail.com>
2022-02-21 19:13:56 +0530

    added getelementbytagname

--- main.js
+++ main.js
@@ -16,9 +16,26 @@
 // }
 
 // GetElementByTagName
-const li = document.getElementsByTagName("li");
-li[2].style.backgroundColor = "green";
+// const li = document.getElementsByTagName("li");
+// li[2].style.backgroundColor = "green";
 
-// this will not work
-const li_class = document.getElementsByClassName("list-group-item");
-li_class[4].style.backgroundColor = "blue"; 
+// // this will not work
+// const li_class = document.getElementsByClassName("list-group-item");
+// li_class[4].style.backgroundColor = "blue"; 
+
+//QuerySelector
+// const li_2 = document.querySelector(".list-group-item:nth-child(1)");
+// li_2.style.backgroundColor = "green";
+
+// const li_3 = document.querySelector(".list-group-item:nth-child(2)");
+// // li_3.style.display = "none";
+
+// // QuerySelectorAll
+// const li = document.querySelectorAll("li");
+// li[1].style.backgroundColor = "green";
+
+// const odd = document.querySelectorAll("li:nth-child(odd)");
+// for(let el of odd){
+//     el.style.backgroundColor = "green";
+// }
+
