600140c576b508a2311987b5a8696c4990025ab9
saurabh <saurabh7481@gmail.com>
2022-02-21 18:54:47 +0530

    added getelementbyclassname

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
-const header = document.querySelector("#header-title");
-header.style.borderBottom = "solid 2px blue";
+// DOM Intro
+// const header = document.querySelector("#header-title");
+// header.style.borderBottom = "solid 2px blue";
 
-const header2 = document.querySelector(".title");
-header2.style.fontWeight = "bold";
-header2.style.color = "green";
+// const header2 = document.querySelector(".title");
+// header2.style.fontWeight = "bold";
+// header2.style.color = "green";
+
+// GetElementByClassName
+const items = document.getElementsByClassName("list-group-item");
+console.log(items);
+items[2].style.backgroundColor = "green";
+
+for(let el of items){
+    el.style.fontWeight = "bold";
+}
