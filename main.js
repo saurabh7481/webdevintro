93838fa672c6aef55cccad63e2d433af5ff61517
saurabh <saurabh7481@gmail.com>
2022-02-21 19:46:56 +0530

    added dom_manipulation

--- main.js
+++ main.js
@@ -39,3 +39,45 @@
 //     el.style.backgroundColor = "green";
 // }
 
+
+// DOM Manipulation
+// const li = document.querySelector('.list-group');
+// console.log(li.parentElement);
+// li.parentElement.style.backgroundColor = "grey";
+
+// console.log(li.lastElementChild);
+// li.lastElementChild.style.backgroundColor = "grey";
+
+// console.log(li.lastChild);
+
+// console.log(li.firstElementChild);
+// li.firstElementChild.style.backgroundColor = "grey";
+
+// console.log(li.firstChild);
+// console.log(li.nextSibling);
+
+// console.log(li.nextElementSibling);
+// li.nextElementSibling.style.backgroundColor = "grey";
+
+// console.log(li.previousSibling);
+
+// console.log(li.previousElementSibling);
+// li.previousElementSibling.style.backgroundColor = "grey";
+
+
+// const newDiv = document.createElement('div');
+// newDiv.setAttribute("title", "hello world");
+
+// const helloText = document.createTextNode("Hello world");
+// newDiv.appendChild(helloText);
+
+// // const container = document.querySelector("header .container");
+// // const h1 = document.querySelector("header h1");
+
+// // container.insertBefore(newDiv, h1);
+
+// const ul = document.querySelector("ul");
+// console.log(ul);
+// const list = document.querySelector("li");
+
+// ul.insertBefore(newDiv, list);
